e7496679f38b5b41eddd3265d90f711ed65911a1
yaden mustopa <brildent@gmail.com>
2022-05-18 22:14:20 +0700

    change name

--- app/Database/Migrations/2022-05-18-123135_Users.php
+++ app/Database/Migrations/2022-05-18-123135_Users.php
@@ -13,13 +13,15 @@ class Users extends Migration
     {
         //
         $sql = "CREATE TABLE IF NOT EXISTS `$this->table`(
-            `id` INT(11) NOT NULL AUTO_INCREMENT,
-            `nama` VARCHAR(20) NULL DEFAULT '0',
+            `id` INT(11) UNSIGNED ZEROFILL NOT NULL AUTO_INCREMENT,
+            `name` VARCHAR(20) NULL DEFAULT '0',
+            `updated_at` INT(15) NULL DEFAULT '0',
+            `created_at` INT(15) NULL DEFAULT '0',
             `username` VARCHAR(20) NULL DEFAULT '0',
             `password` VARCHAR(50) NULL DEFAULT '0',
             `status` ENUM('ADMIN','KARYAWAN') NULL DEFAULT 'KARYAWAN',
             PRIMARY KEY (`id`),
-            INDEX `nama` (`nama`),
+            INDEX `name` (`name`),
             INDEX `status` (`status`)
         )";
 
