a8d41c7a25e1c0de1388e76ab57a275b0af99c6e
yaden <brildent@gmail.com>
2022-05-19 15:30:01 +0700

    change structure absens table

--- app/Database/Migrations/2022-05-18-123214_Absens.php
+++ app/Database/Migrations/2022-05-18-123214_Absens.php
@@ -11,13 +11,14 @@ class Absens extends Migration
     public function up()
     {
         $sql = "CREATE TABLE IF NOT EXISTS `$this->table` (
-            `id` INT(11) NOT NULL,
-            `users_id` INT(11) NULL DEFAULT NULL,
+            `id` INT(11) NOT NULL AUTO_INCREMENT,
+            `user_id` INT(11) NULL DEFAULT NULL,
+            `status` ENUM('MASUK','TANPA_KETERANGAN','CUTI','IZIN') NULL DEFAULT 'MASUK' COLLATE 'latin1_swedish_ci',
+            `description` LONGTEXT NULL DEFAULT NULL COLLATE 'latin1_swedish_ci',
             `created_at` INT(11) NULL DEFAULT NULL COMMENT 'use time stamp',
-            `status` ENUM('MASUK','TANPA_KETERANGAN','CUTI','IZIN') NULL DEFAULT 'MASUK',
-            `description` LONGTEXT NULL DEFAULT NULL,
-            PRIMARY KEY (`id`),
-            INDEX `status` (`status`)
+            `updated_at` INT(11) NULL DEFAULT NULL,
+            PRIMARY KEY (`id`) USING BTREE,
+            INDEX `status` (`status`) USING BTREE
         )";
 
         $this->db->query( $sql );
