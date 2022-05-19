9e75675db38f8f92c49fae89539e8c68fdc4b2da
yaden mustopa <brildent@gmail.com>
2022-05-19 08:25:14 +0700

    change karyawan_id to user_id

--- app/Database/Migrations/2022-05-18-123214_Absens.php
+++ app/Database/Migrations/2022-05-18-123214_Absens.php
@@ -10,10 +10,10 @@ class Absens extends Migration
 
     public function up()
     {
-        $sql = "CREATE TABLE IF NOT EXISTS `$this->table`(
+        $sql = "CREATE TABLE IF NOT EXISTS `$this->table` (
             `id` INT(11) NOT NULL,
-            `karyawan_id` INT(11) NULL DEFAULT NULL,
-            `created_at` INT(15) NULL DEFAULT NULL COMMENT 'use time stamp',
+            `users_id` INT(11) NULL DEFAULT NULL,
+            `created_at` INT(11) NULL DEFAULT NULL COMMENT 'use time stamp',
             `status` ENUM('MASUK','TANPA_KETERANGAN','CUTI','IZIN') NULL DEFAULT 'MASUK',
             `description` LONGTEXT NULL DEFAULT NULL,
             PRIMARY KEY (`id`),
