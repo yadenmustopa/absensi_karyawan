e1d772fd85fa79b0b93bfa84a4b1237dbc31d09d
yaden <brildent@gmail.com>
2022-05-19 15:28:32 +0700

    crate absent

--- app/Controllers/Absens.php
+++ app/Controllers/Absens.php
@@ -2,18 +2,123 @@
     namespace App\Controllers;
 
     use CodeIgniter\Controller;
-    use App\Models\KaryawansModel;
+    use App\Models\UsersModel;
+    use App\Models\AbsensModel;
     use CodeIgniter\I18n\Time;
 
-    class Absens extends BaseController
+    class Absens extends ApiController
     {
         public function index()
         {
-            $name        = $this->request->getGet('name');
-            $start_date  = $this->request->getGet('start_date');
-            $end_date    = $this->request->getGet('end_date');
+            $search        = $this->request->getGet('search') ;
+            $start_date    = $this->request->getGet('start_date') ?? strtotime( date('m-01-Y 00:00:00' ) );
+            $end_date      = $this->request->getGet('end_date') ??  Time::now('Asia/Jakarta','id')->getTimestamp();
 
+            $$search = trim( $search );
+
+            $users_model = new UsersModel();
+            $base = "SELECT `users`.`name` AS `name`,`absens`.`created_at` AS `created_at`,`absens`.`status` AS `status`,`absens`.`description` AS `description` FROM `users` JOIN `absens` ON `absens`.`user_id` = `users`.`id`";
+
+            $base.= " WHERE `absens`.`created_at` BETWEEN $start_date AND $end_date";
+            if( $search ){
+                $base .= " AND `users`.`name` LIKE '%$search%' OR  `absens`.`status` LIKE '% $search%'  ";
+            }
+
+            var_dump($base);
             
+            $sql = $users_model->db->query( $base );
+
+            $response = $sql->getResult('array');
+
+            $data = ["data" => $response ];
+            return $this->successOutput( $data, 200 ) ;
+        }
+
+
+        public function add()
+        {
+            $rules       = $this->getRules();
+            $validation  = $this->validation( $rules );
+
+            if( ! $validation["success"] ) return $this->errorOutput( $validation['message'] );
+
+            $user_id      = $this->request->getPost("user_id");
+            $status       = $this->request->getPost("status");
+            $description  = $this->request->getPost("description");
+            $now          = Time::now('Asia/Jakarta','id')->getTimestamp();
+
+
+            $data = [ 
+                "user_id"   => $user_id,
+                "status"    => $status,
+                "description" => $description,
+                "created_at" => $now,
+            ];
+
+            $AbsenModel = new AbsensModel();
+            $AbsenModel->insert( $data );
+
+            return $this->successOutput([]);
+        }
+
+        public function update()
+        {
+            $rules       = $this->getRules();
+            $validation  = $this->validation( $rules );
+
+            if( ! $validation["success"] ) return $this->errorOutput( $validation['message'] );
+
+            $user_id      = $this->request->getPost("user_id");
+            $status       = $this->request->getPost("status");
+            $description  = $this->request->getPost("description");
+
+            $data = [ 
+                "user_id"   => $user_id,
+                "status"    => $status,
+                "description" => $description,
+            ];
+
+            $AbsenModel = new AbsensModel();
+            $AbsenModel->insert( $data );
+
+            return $this->successOutput([]);
+        }
+
+
+        public function getRules()
+        {
+            return [
+                'user_id' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        "required" => "{field} tidak boleh kosong",
+                       
+                    ]
+                ],
+                'status' => [
+                    'rules' => 'required',
+                    'errors' => [
+                        "required" => "{field} tidak boleh kosong",
+                    ]
+                ],
+            ];
+        }
+
+
+        public function validation( $rules ){
+
+            if( $this->validate( $rules )){
+
+                return ['success'=> true ];
+                
+            }else{
+
+                $validation = \Config\Services::validation();
+                $errors  = array_values( $validation->getErrors() );
+                $message = $errors[0];
+
+                return ['success'=> false, 'message' => $message ];
+            }
         }
     }
 
