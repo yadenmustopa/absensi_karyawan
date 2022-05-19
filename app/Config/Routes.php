092ef07d9796b5efb58d7809116ad974aa934367
yaden mustopa <brildent@gmail.com>
2022-05-19 08:23:08 +0700

    add Routes cange pswd

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,22 +35,23 @@ $routes->get('/', 'Home::index');
 
 //route for management login
 $routes->get('/login','Login::index');
-$routes->post('/login','Login::auth');
+$routes->post('/auth','Login::auth');
 
 $routes->get('/karyawans', 'Karyawans::index');
+$routes->post( 'karyawans/(:num)', 'Karyawans::upload/$1');
 $routes->post('/karyawans', 'Karyawans::add');
-$routes->put('/karyawans/(:num)', 'Karyawans::update');
-$routes->patch( 'karyawans/(:num)', 'Karyawans::upload');
-$routes->delete( 'karyawans/(:num)', 'Karyawans::delete');
+$routes->put('/karyawans/(:num)', 'Karyawans::update/$1');
+$routes->delete( 'karyawans/(:num)', 'Karyawans::delete/$1');
 
 $routes->get('/users', 'Users::index');
 $routes->post('/users', 'Users::add');
-$routes->put('/users/(:num)', 'Users::update');
-$routes->delete('/users/(:num)', 'Users::delete');
+$routes->put('/users', 'Users::update');
+$routes->put('/change_pwd/', 'Users::updatePwd');
+$routes->delete('/users/(:num)', 'Users::delete/$1');
 
 $routes->get('/absens', 'Absens::index');
 $routes->post('/absens', 'Absens::add');
-$routes->put('/absens/(:num)', 'Absens::update');
+$routes->put('/absens/(:num)', 'Absens::update/$1');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
