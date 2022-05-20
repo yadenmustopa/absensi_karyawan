e3c9f67e82bb22f7ec012bbf6e7b298e1e6acd5f
yaden mustopa <brildent@gmail.com>
2022-05-20 09:53:13 +0700

    change Login to check db

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -26,20 +26,11 @@
                 return $this->errorOutput(  'username/password jangan ada yang kosong' );
             }
             
-            $users_model = new UsersModel();
             $username    = $this->request->getPost("username");
             $password    = $this->request->getPost("password");
-
-
-
-            $data = [ 
-                'username' => $username,
-                'password' => sha1( $password ),
-            ];
-            
-            $auth = $users_model->where( $data )->find();
-
-            if( ! $auth ){
+            $check       = $this->checkInDb( $username, $password );
+          
+            if( ! $check["success"] ){
                 return $this->errorOutput('Kesalahan Credential');
             }
 
@@ -47,8 +38,28 @@
             $api_key = ApiKey::generate( $username, $password );
 
 
-            $data = [ "api_key" => $api_key ];
-            return $this->successOutput( $auth, );
+            $data = [ "api_key" => $api_key, "data" => $check["data"] ];
+            return $this->successOutput( $data );
+        }
+
+        /**
+         * 
+         * @return []
+         */
+        public function checkInDb( $username, $password ){
+            $data = [ 
+                'username' => $username,
+                'password' => sha1( $password ),
+            ];
+
+            $users_model = new UsersModel();
+            $auth = $users_model->where( $data )->find();
+
+            if( !$auth ){
+                return ['success' => false ];
+            }else{
+                return ["success" => true , "data" => $auth ];
+            }
         }
 
 
