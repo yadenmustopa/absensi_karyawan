73429b8530f4871ffff825ca7aab32b86707c134
yaden mustopa <brildent@gmail.com>
2022-05-20 09:53:43 +0700

    handle check api_key

--- app/Filters/CheckApiKey.php
+++ app/Filters/CheckApiKey.php
@@ -5,21 +5,53 @@
     use CodeIgniter\HTTP\ResponseInterface;
     use CodeIgniter\HTTP\Message;
     use CodeIgniter\Filters\FilterInterface;
+    use App\Controllers\Login;
+    use App\Controllers\ApiController;
+    use App\Libraries\ApiKey;
+use Exception;
 
     class CheckApiKey implements FilterInterface
     {
         public function before( RequestInterface $request, $arguments =  null)
         {
             $path = uri_string(true);   
-
-            if( $path !== 'login' || $path !== 'auth' ){
-                $message = new Message();
-                $header  = $message->getHeaderLine('api-key');
-                var_dump($header);
+            
+            if( $path === 'login' OR $path === 'auth' OR $path === '' ){
+                
+            }else{
+                $ApiController   = new ApiController();
+                $api_key  = $_SERVER['HTTP_API_KEY'];
+                if( !$api_key || ! $_SERVER['HTTP_API_KEY'] ) return $ApiController->errorOutput("tidak di ijinkan mengakses", 401 );
+                
+                
+                $check = $this->checkApiKey( $api_key );
+                if( ! $check ) return $ApiController->errorOutput('key tidak berlaku', 401 );
+                
             }
 
+        }
 
-            var_dump( $path );
+        /**
+         * @param string api key
+         * 
+         */
+        private function checkApiKey( $api_key ){
+  
+            $Login           = new Login();
+
+            try{
+
+                $data            = ApiKey::translate( $api_key );
+                $username        = $data["username"];
+                $password        = $data["password"];
+                $check = $Login->checkInDb( $username, $password );
+
+            }catch( Exception $e ){
+                return false;
+            }
+            
+
+            return $check["success"];
         }
 
         public function after( RequestInterface $request, ResponseInterface $response, $arguments =  null)
