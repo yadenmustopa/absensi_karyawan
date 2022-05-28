7f0f77579dfdf147b7639ab2f4d17194e7cb4240
yaden <brildent@gmail.com>
2022-05-28 12:46:29 +0700

    add header access-control-allow-origin

--- app/Filters/CheckApiKey.php
+++ app/Filters/CheckApiKey.php
@@ -14,6 +14,17 @@ use Exception;
     {
         public function before( RequestInterface $request, $arguments =  null)
         {
+            header('Access-Control-Allow-Origin: NULL');
+            header('Access-Control-Allow-Origin: *');
+            header("Access-Control-Allow-Headers: HTTP_KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+            header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, PATCH, DELETE");
+
+            $method = $_SERVER['REQUEST_METHOD'];
+      
+            if ($method == "OPTIONS") {
+            die();
+            }
+
             $path = uri_string(true);   
             
             if( $path === 'login' OR $path === 'auth' OR $path === '' ){
